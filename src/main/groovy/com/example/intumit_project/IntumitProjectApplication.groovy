62b4102213fc61d62f4fedb3ceefb7771726f95f
yuwei0713 <ads9210155@gmail.com>
2025-03-24 16:15:57 +0800

    exclude all jpa

--- src/main/groovy/com/example/intumit_project/IntumitProjectApplication.groovy
+++ src/main/groovy/com/example/intumit_project/IntumitProjectApplication.groovy
@@ -2,13 +2,11 @@ package com.example.intumit_project
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [HibernateJpaAutoConfiguration])
 class IntumitProjectApplication {
-
     static void main(String[] args) {
         SpringApplication.run(IntumitProjectApplication, args)
-        System.out.print("--------------test--------------")
     }
-
 }
